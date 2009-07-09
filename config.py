24b02ed443d81c5124b1a59dd33003f5ee6d4e5b
DeFirence <joshuajay@gmail.com>
2009-07-09 18:59:21 +0200

    cleaned up config a little (procrastinating)

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#default IRC configs
+# IRC config
 class irc:
     server      = "za.shadowfire.org"
     port        = 6667
@@ -8,13 +8,11 @@ class irc:
     nickname    = ("pyWerewolf", "pyWerewolf_")
     password    = "werewolf"
 
-#default game configs
+# Game config
 class game:
-    signup = 90 #signup time
-    cycle = {}
-    cycle['day'] =  120 #length of day cycle
-    cycle['night'] = 60 #length of night cycle
-    min_players =  6 #minimum amount of players for game
-    max_players = 42 #maximum amount of players for game
-
-
+    signup          = 90  # signup time
+    cycle           = {}
+    cycle['day']    = 120 # length of day cycle
+    cycle['night']  = 60  # length of night cycle
+    min_players     = 6   # minimum amount of players for game
+    max_players     = 42  # maximum amount of players for game
