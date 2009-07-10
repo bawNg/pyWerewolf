25b4422a16eb8505a006217599fbd61e6af49c42
ShadowMaster1991 <kosie.vandermerwe@gmail.com>
2009-07-10 12:07:49 +0200

    fixed up game_data.py

--- game_data.py
+++ game_data.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
 
-### Game Text Messages ###
-#$user gets replaced by the current user
-#$target gets replaced by who is targeted by current user
-#$votes gets replaced by current vote tallies
-#$wolves gets the names of the wolves
-#$alive gets the alive villagers
+class Theme:
+    ### Game Text Messages ###
+    #$user gets replaced by the current user
+    #$target gets replaced by who is targeted by current user
+    #$votes gets replaced by current vote tallies
+    #$wolves gets the names of the wolves
+    #$alive gets the alive villagers
 
-#message when game starts
-game_starts_message = 
+    #message when game starts
+    game_starts_message = ["$user started a new game!"]
 
-#message when someone new joins hunt
-join_new_message = ["$user joined the hunt!"]
+    #message when someone new joins hunt
+    join_new_message = ["$user joined the hunt!"]
