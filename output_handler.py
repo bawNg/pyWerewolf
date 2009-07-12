9f47778fa03097483246a3d841cdfa198c5d04e7
ShadowMaster1991 <kosie.vandermerwe@gmail.com>
2009-07-12 23:16:33 +0200

    Setup theme_handler in output_handler.py

--- output_handler.py
+++ output_handler.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from theme_handler import ThemeHandler
+from theme import WerewolfTheme
+
 class MessageType:
     chan        = 0 #Message that will be sent to the channel
     notice_p    = 1 #Notice that will be sent to the player
@@ -8,6 +11,8 @@ class MessageType:
 class OutputHandler:
     def __init__(self, bot):
         self.irc = bot
+        self.theme_handler = ThemeHandler()
+        self.theme_handler.set_theme(WerewolfTheme())
 
     
 
